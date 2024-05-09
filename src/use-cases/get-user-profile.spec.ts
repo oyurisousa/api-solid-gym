4e15c31b4b4f2478a6e175ceded4271088141037
yuri <yurisousaenfer@gmail.com>
2024-05-09 06:10:54 -0300

    feat: get user by id use case

---
+++ src/use-cases/get-user-profile.spec.ts
@@ -0,0 +1,37 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
+import { hash } from 'bcryptjs'
+import { UsersRepository } from '@/repositories/users-repository'
+import { GetUserProfileUseCase } from './get-user-profile'
+import { ResourceNotFoundError } from './erros/resource-not-found-error'
+
+let usersRepository: UsersRepository
+let sut: GetUserProfileUseCase
+
+describe('Get user profile Use case', () => {
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new GetUserProfileUseCase(usersRepository)
+  })
+
+  it('should be able to get user profile', async () => {
+    const createdUser = await usersRepository.create({
+      name: 'yuri',
+      email: 'yuri@gmail.com',
+      password_hash: await hash('Iruysousa', 6),
+    })
+
+    const { user } = await sut.execute({
+      userId: createdUser.id,
+    })
+
+    expect(user.email).toEqual('yuri@gmail.com')
+  })
+  it('should not be able to get user profile with wrong id', async () => {
+    expect(() =>
+      sut.execute({
+        userId: 'non-existing-id',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  })
+})
