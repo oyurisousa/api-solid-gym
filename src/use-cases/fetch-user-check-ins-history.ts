451b6bf57e2126ca00e56ad321ac9068859ca030
yuri <yurisousaenfer@gmail.com>
2024-05-18 17:57:12 -0300

    feat: fetch user check-ins history [feat & test]

---
+++ src/use-cases/fetch-user-check-ins-history.ts
@@ -0,0 +1,29 @@
+import { CheckIn } from '@prisma/client'
+import { CheckInsRepository } from '@/repositories/check-in-repository'
+
+interface FetchUserCheckInsHistoryUseCaseRequest {
+  userId: string
+  page: number
+}
+
+interface FetchUserCheckInsHistoryUseCaseResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInsHistory {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    page,
+  }: FetchUserCheckInsHistoryUseCaseRequest): Promise<FetchUserCheckInsHistoryUseCaseResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(
+      userId,
+      page,
+    )
+
+    return {
+      checkIns,
+    }
+  }
+}
