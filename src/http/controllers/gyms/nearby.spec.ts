e030c3c0e262f41de4696b27605f68c10053d83f
yuri <yurisousaenfer@gmail.com>
2024-06-04 19:52:42 -0300

    test: neraby gym controller

---
+++ src/http/controllers/gyms/nearby.spec.ts
@@ -0,0 +1,53 @@
+import { app } from '@/app'
+import { createAndAuthenticateUser } from '@/utils/test/create-and-authenticate-user'
+import request from 'supertest'
+
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+
+describe('Nearby Gym (e2e)', () => {
+  beforeAll(() => {
+    app.ready()
+  })
+  afterAll(() => {
+    app.close()
+  })
+
+  it('shoule be able to list nearby gyms', async () => {
+    const { token } = await createAndAuthenticateUser(app)
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'ZeroAll',
+        description: 'the best, from zero to one',
+        phone: '(88) 99999-8888',
+        latitude: -2.8915351,
+        longitude: -41.6468856,
+      })
+
+    await request(app.server)
+      .post('/gyms')
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        title: 'ZeroOne',
+        description: 'the best, from zero to one',
+        phone: '(88) 99999-8888',
+        latitude: -2.9271375,
+        longitude: -41.7506933,
+      })
+
+    const response = await request(app.server)
+      .get('/gyms/nearby')
+      .query({
+        latitude: -2.9238915,
+        longitude: -41.7506933,
+      })
+      .set('Authorization', `Bearer ${token}`)
+      .send()
+
+    console.log(response)
+    expect(response.statusCode).toEqual(200)
+    // expect(response.body.gyms).toHaveLength(1)
+  })
+})
